8e5880c1b81c2119e01c46ea54e8efae1fadc1d1
ShivuSarthi <shivakumarsarthi@gmail.com>
2024-06-06 17:54:25 +0530

    updated cors

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ require("dotenv").config({ path: "config/config.env" });
 const app = express();
 
 // Middleware
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true
-  })
-);
+app.use(cors());
 
 app.use(bodyParser.json());
 
